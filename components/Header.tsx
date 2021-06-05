9699ea0c4ff3e70962ad4fd82a31808402ce1ef7
Misha Moroshko <michael.moroshko@gmail.com>
2021-06-05 11:47:43 +1000

    Add viewport meta tag

--- components/Header.tsx
+++ components/Header.tsx
@@ -65,6 +65,10 @@ export default function Header({ pageTitle, pageDescription }: Props) {
   return (
     <>
       <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
         <title>{title}</title>
         <meta name="description" content={pageDescription} />
         <link rel="icon" href="/favicon.png" />
