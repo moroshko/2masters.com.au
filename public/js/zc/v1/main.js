e9ec256b8893ebb5b9dca1cd7c32bec8ee0d5176
Misha Moroshko <michael.moroshko@gmail.com>
2021-06-13 13:58:10 +1000

    Update zc files

--- public/js/zc/v1/main.js
+++ public/js/zc/v1/main.js
@@ -1,5 +1,42 @@
-const p = document.createElement("p");
-p.className = "app";
-p.append("Hello from Zones Controller");
+document.title = "Zones Controller";
 
-document.body.append(p);
+// Delete existing HTML
+document
+  .querySelectorAll("article > :not(script)")
+  .forEach((el) => el.remove());
+
+// Add React root element
+const div = document.createElement("div");
+div.id = "root";
+
+document.body.append(div);
+
+// Add React
+let script = document.createElement("script");
+script.src = "https://unpkg.com/react@17.0.2/umd/react.production.min.js";
+script.crossOrigin = "anonymous";
+document.body.append(script);
+// Add ReactDOM
+script = document.createElement("script");
+script.src =
+  "https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js";
+script.crossOrigin = "anonymous";
+document.body.append(script);
+// Add dataformsjs
+script = document.createElement("script");
+script.src =
+  "https://cdn.jsdelivr.net/npm/dataformsjs@5.9.0/js/react/jsxLoader.min.js";
+document.body.append(script);
+
+// Add app code
+script = document.createElement("script");
+script.type = "text/babel";
+script.innerHTML = `
+  function App() {
+    return (
+      <h1>Hello World!</h1>
+    )
+  }
+
+  ReactDOM.render(<App />, document.getElementById("root"));
+`;
