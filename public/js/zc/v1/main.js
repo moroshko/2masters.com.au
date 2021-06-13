83459ea3e9212fb7bbdef11be28a1b152c73d0f6
Misha Moroshko <michael.moroshko@gmail.com>
2021-06-13 21:23:05 +1000

    Update zc files

--- public/js/zc/v1/main.js
+++ public/js/zc/v1/main.js
@@ -10,26 +10,41 @@ const div = document.createElement("div");
 div.id = "root";
 document.body.append(div);
 
-// Add React
-let script = document.createElement("script");
-script.src = "https://unpkg.com/react@17.0.2/umd/react.production.min.js";
-script.crossOrigin = "anonymous";
-document.body.append(script);
-// Add ReactDOM
-script = document.createElement("script");
-script.src =
-  "https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js";
-script.crossOrigin = "anonymous";
-document.body.append(script);
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement("script");
+    script.src = src;
+    script.onload = resolve;
+    script.onerror = reject;
+    script.crossOrigin = "anonymous";
+    document.body.append(script);
+  });
+}
 
-// Add app code
-script = document.createElement("script");
-script.innerHTML = `
-  const app = React.createElement(
-    "div",
-    "Hello World",
+(async () => {
+  await Promise.all([
+    loadScript("https://unpkg.com/react@17.0.2/umd/react.production.min.js"),
+    loadScript(
+      "https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js"
+    ),
+  ]);
+  ReactDOM.render(
+    /*#__PURE__*/ React.createElement(App, null),
+    document.getElementById("root")
   );
+})();
 
-  ReactDOM.render(app, document.getElementById("root"));
-`;
-document.body.append(script);
+function App() {
+  React.useEffect(() => {
+    const eventSource = new EventSource("/events");
+
+    eventSource.onmessage = (event) => {
+      console.log(event);
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+  return /*#__PURE__*/ React.createElement("p", null, "Hello world!!");
+}
