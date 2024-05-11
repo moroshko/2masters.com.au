60d3466c37dc6972827e6856a17a6d67d720372d
Misha Moroshko <michael.moroshko@gmail.com>
2024-05-11 21:32:18 +1000

    Better error handling

--- app/pay/components/PayPageContent/PayForm.tsx
+++ app/pay/components/PayPageContent/PayForm.tsx
@@ -45,29 +45,35 @@ const PayForm = () => {
     setIsLoading(true);
     setErrorMessage(null);
 
-    const { error } = await stripe.confirmPayment({
-      elements,
-      confirmParams: {
-        payment_method_data: {
-          billing_details: {
-            address: {
-              country: "AU",
+    try {
+      const { error } = await stripe.confirmPayment({
+        elements,
+        confirmParams: {
+          payment_method_data: {
+            billing_details: {
+              address: {
+                country: "AU",
+              },
             },
           },
+          return_url: getReturnUrl(),
         },
-        return_url: getReturnUrl(),
-      },
-    });
+      });
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
-      setErrorMessage(error.message ?? "Unexpected error");
-    } else {
-      setErrorMessage("An unexpected error occurred.");
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Otherwise, your customer will be redirected to
+      // your `return_url`. For some payment methods like iDEAL, your customer will
+      // be redirected to an intermediate site first to authorize the payment, then
+      // redirected to the `return_url`.
+      if (error.type === "card_error" || error.type === "validation_error") {
+        setErrorMessage(error.message ?? "Unexpected error");
+      } else {
+        setErrorMessage("An unexpected error occurred.");
+      }
+    } catch (error) {
+      setErrorMessage(
+        error instanceof Error ? error.message : "Payment failed"
+      );
     }
 
     setIsLoading(false);
